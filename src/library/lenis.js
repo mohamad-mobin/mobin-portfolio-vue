0126ff94b90a2e4cfdbb118672ae1ca1fb32ecfb
MohamadMobin Rezaee <134555495+ALONE0007@users.noreply.github.com>
2025-09-12 16:28:59 +0330

    updating lenis

--- src/library/lenis.js
+++ src/library/lenis.js
@@ -6,6 +6,8 @@ let rafId = null
 export function initLenis(options = {}) {
   if (lenis) return lenis // دوباره initialize نشه
 
+  const isMobile = /Mobi|Android/i.test(navigator.userAgent)
+
   lenis = new Lenis({
     duration: 2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
@@ -13,8 +15,8 @@ export function initLenis(options = {}) {
     gestureDirection: "vertical",
     smooth: true,
     mouseMultiplier: 1.2,
-    smoothTouch: true,
-    touchMultiplier: 2,
+    smoothTouch: !isMobile,          // دسکتاپ: true, موبایل: false
+    touchMultiplier: isMobile ? 1 : 2,
     infinite: false,
     ...options, // اگه خواستی override کنی
   })
@@ -27,20 +29,19 @@ export function initLenis(options = {}) {
 
   // anchor links
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", (e) => {
-    e.preventDefault()
-    const targetId = anchor.getAttribute("href")
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault()
+      const targetId = anchor.getAttribute("href")
 
-    if (targetId === "#" || targetId === "") {
-      // ✅ بره بالای صفحه
-      lenis.scrollTo(0)
-    } else {
-      // ✅ بره به سکشن مورد نظر
-      lenis.scrollTo(targetId)
-    }
+      if (targetId === "#" || targetId === "") {
+        // ✅ بره بالای صفحه
+        lenis.scrollTo(0)
+      } else {
+        // ✅ بره به سکشن مورد نظر
+        lenis.scrollTo(targetId)
+      }
+    })
   })
-})
-
 
   return lenis
 }
