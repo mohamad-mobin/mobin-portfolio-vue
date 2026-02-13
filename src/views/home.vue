5d2a03feb0998170cb1d13974e386350d7267f4e
mohamad-mobin <www.mohamadmobin1385@gmail.com>
2026-02-13 20:21:49 +0330

    come back to project

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,6 +4,13 @@
         <Cursor />
         <toggleLang/>
     
+
+                <!-- testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt -->
+        <div class="fixed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem architecto eaque soluta accusamus atque itaque debitis quia aut, dolorum odit rerum reprehenderit nihil, fugit assumenda nostrum nam, corporis quibusdam. Dolorum.
+        </div>
+        <!-- testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt -->
+
             <div class="container mx-auto pt-5 relative">
             <div class="md:fixed absolute w-[400px] h-[300%] left-[30%] -top-[190%] rotate-45 md:-top-[10%] md:-left-[30%] md:w-[51%] md:h-[200%] md:-rotate-[12deg] z-10  bg-[#FFB400]"></div>
     
