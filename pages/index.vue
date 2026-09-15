de5c76e466bb5668e6bf48df77f2a8d5088fb0ff
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-15 18:17:36 +0330

    test: use vue-i18n composable in SSR

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h1>{{ $t('hello') }}</h1>
+    <h1>{{ t('hello') }}</h1>
   </div>
 </template>
 
-<script>
-export default {}
+<script setup>
+const { t } = useI18n()
 </script>
