b591897c1ec5b301fdbf8b91a6f60e99400be33e
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-15 18:10:23 +0330

    test: isolate vue-i18n instance creation

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,5 +1,15 @@
 import { createI18n } from 'vue-i18n'
 
 export default defineNuxtPlugin(() => {
-  console.log('vue-i18n imported successfully')
+  const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    messages: {
+      en: {
+        hello: 'Hello'
+      }
+    }
+  })
+
+  console.log('i18n instance created:', !!i18n)
 })
