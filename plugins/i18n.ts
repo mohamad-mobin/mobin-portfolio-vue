6fb038dcf1cfa47f5a85644ca39bc13fbd58dba8
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-15 15:53:37 +0330

    refactor: migrate i18n to composition API

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -4,7 +4,7 @@ import fa from '~/i18n/locales/fa.json'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const i18n = createI18n({
-    legacy: true,
+    legacy: false,
     locale: 'en',
     fallbackLocale: 'en',
     messages: {
