f54b3c594a02b28fc72c69526c75923895cf4d5c
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-15 15:08:12 +0330

    fix : i18n bad loading

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,12 +1,19 @@
 import { createI18n } from 'vue-i18n'
+import en from '~/i18n/locales/en.json'
+import fa from '~/i18n/locales/fa.json'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const i18n = createI18n({
     legacy: true,
     locale: 'en',
     fallbackLocale: 'en',
-    messages: {}
+    messages: {
+      en,
+      fa
+    }
   })
 
   nuxtApp.vueApp.use(i18n)
+
+  console.log('✅ Translations loaded')
 })
