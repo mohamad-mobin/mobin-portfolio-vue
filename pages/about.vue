c7a96ebdc2f90a9370ec32aa3e6ef2d2d329edce
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-17 20:05:58 +0330

    update : add more tools & rename tools-image folder to tools&techs-image

--- pages/about.vue
+++ pages/about.vue
@@ -254,7 +254,95 @@
                                     :techName="'GitHub'" />
                         </div>
                     </div>
+
+                    <div class="flex gap-5">
+                        <div class="hover:scale-110 duration-300">
+                            <Tools&techs
+                                        hoverBorder="hover:border-white/70"
+                                        boxGradiant="bg-gradient-to-br from-white/10 to-transparent" 
+                                        :imgSrc="linuxImg" 
+                                        :percent="80" 
+                                        :techName="'Linux'" />
+                        </div>
+                        <div class="hover:scale-110 duration-300">
+
+                            <Tools&techs
+                                        hoverBorder="hover:border-orange-400"
+                                        boxGradiant="bg-gradient-to-br from-orange-300/10 to-transparent" 
+                                        :imgSrc="ubuntuImg" 
+                                        :percent="80" 
+                                        :techName="'Ubuntu'" />
+                        </div>
+
+                    </div>
         </div>
+
+
+        <div style="font-family: poppins;" class="flex flex-wrap justify-center gap-5 px-5 pt-5  md:pb-5 text-center text-white max-w-7xl mx-auto z-50">
+
+            
+                    <div class="flex gap-5">
+                        <div class="hover:scale-110 duration-300">
+            
+                            <Tools&techs
+                                    hoverBorder="hover:border-blue-500"
+                                    boxGradiant="bg-gradient-to-br from-blue-500/10 to-transparent" 
+                                    :imgSrc="vscodeImg" 
+                                    :percent="98" 
+                                    :techName="'VS Code'" />
+            
+                        </div>
+            
+                        <div class="hover:scale-110 duration-300">
+            
+                            <Tools&techs
+                                    hoverBorder="hover:border-orange-400"
+                                    boxGradiant="bg-gradient-to-br from-orange-500/10 to-transparent" 
+                                    :imgSrc="postmanImg" 
+                                    :percent="60" 
+                                    :techName="'Postman'" />
+                        </div>
+                    </div>
+
+                    <div class="flex gap-5">
+                        <div class="hover:scale-110 duration-300">
+                            <Tools&techs
+                                        hoverBorder="hover:border-green-500/70"
+                                        boxGradiant="bg-gradient-to-br from-green-500/10 to-transparent" 
+                                        :imgSrc="figmaImg" 
+                                        :percent="80" 
+                                        :techName="'Figma'" />
+                        </div>
+                        <div class="hover:scale-110 duration-300">
+
+                            <Tools&techs
+                                        hoverBorder="hover:border-purple-400"
+                                        boxGradiant="bg-gradient-to-br from-purple-500/10 to-transparent" 
+                                        :imgSrc="bashscriptImg" 
+                                        :percent="80" 
+                                        :techName="'Bash Script'" />
+                        </div>
+
+                    </div>
+        </div>
+
+         <div style="font-family: poppins;" class="flex flex-wrap justify-center gap-5 px-5 pt-5 pb-28 md:pb-5 text-center text-white max-w-7xl mx-auto z-50">
+
+            
+                    <div class="flex gap-5">
+                        <div class="hover:scale-110 duration-300">
+            
+                            <Tools&techs
+                                    hoverBorder="hover:border-green-500"
+                                    boxGradiant="bg-gradient-to-br from-green-500/10 to-transparent" 
+                                    :imgSrc="ffmpegImg" 
+                                    :percent="88" 
+                                    :techName="'FFmpeg'" />
+            
+                        </div>
+                    </div>
+        </div>
+
 
     
      
@@ -310,16 +398,23 @@ import { stopLenis, startLenis } from '@/utils/lenis.js'
 import MiniSideBar from "../components/miniSideBar.vue"
 import TheTitle from "../components/theTitle.vue"
 
-import htmlImg from '~/assets/images/tools-image/html.png'
-import cssImg from '~/assets/images/tools-image/css.png'
-import tailwindImg from '~/assets/images/tools-image/tailwind.png'
-import javascriptImg from '~/assets/images/tools-image/js.png'
-import vueImg from '~/assets/images/tools-image/vue.png'
-import vuetifyImg from '~/assets/images/tools-image/vuetify.png'
-import nuxtImg from '~/assets/images/tools-image/nuxt.png'
-import typescriptImg from '~/assets/images/tools-image/typescript.png'
-import gitImg from '~/assets/images/tools-image/git.png'
-import githubImg from '~/assets/images/tools-image/github.png'
+import htmlImg from '~/assets/images/tools&techs-image/html.png'
+import cssImg from '~/assets/images/tools&techs-image/css.png'
+import tailwindImg from '~/assets/images/tools&techs-image/tailwind.png'
+import javascriptImg from '~/assets/images/tools&techs-image/js.png'
+import vueImg from '~/assets/images/tools&techs-image/vue.png'
+import vuetifyImg from '~/assets/images/tools&techs-image/vuetify.png'
+import nuxtImg from '~/assets/images/tools&techs-image/nuxt.png'
+import typescriptImg from '~/assets/images/tools&techs-image/typescript.png'
+import gitImg from '~/assets/images/tools&techs-image/git.png'
+import githubImg from '~/assets/images/tools&techs-image/github.png'
+import linuxImg from '~/assets/images/tools&techs-image/linux.png'
+import ubuntuImg from '~/assets/images/tools&techs-image/ubuntu.png'
+import vscodeImg from '~/assets/images/tools&techs-image/vscode.png'
+import postmanImg from '~/assets/images/tools&techs-image/postman.png'
+import figmaImg from '~/assets/images/tools&techs-image/figma.png'
+import bashscriptImg from '~/assets/images/tools&techs-image/bashscript.png'
+import ffmpegImg from '~/assets/images/tools&techs-image/ffmpeg.png'
 
 
 const currentPageConst = "about"
