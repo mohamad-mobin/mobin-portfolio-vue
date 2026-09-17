31a729afbcceb0b511d94087cdbc2a099e7ff2a1
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-17 21:06:23 +0330

    update : change size of my name and job element tag

--- pages/index.vue
+++ pages/index.vue
@@ -40,14 +40,14 @@
         <h1 class="group duration-300 text-left leading-tight uppercase font-black tracking-wider">
             <span
                 id="mobin"
-                class="block text-[#FFB400] text-4xl md:text-[30px] lg:text-[4vw] relative duration-300 group-hover:-translate-x-5 group-hover:scale-95
+                class="block text-[#FFB400] text-4xl md:text-[30px] lg:text-[5vw] relative duration-300 group-hover:-translate-x-5 group-hover:scale-95
                     before:absolute before:hidden md:before:block before:h-1 before:w-7 lg:before:w-10 before:bg-[#FFB400] before:rounded-2xl before:bottom-6 lg:before:bottom-8 before:-left-10 lg:before:-left-16"
                 style="font-family: lalezar;"
             >
                 {{ $t('imMobinRezaee') }}
             </span>
             <span
-                class="block text-white text-[25px] lg:text-[2.5vw] duration-300 group-hover:translate-x-10"
+                class="block text-white text-[25px] lg:text-[3vw] duration-300 group-hover:translate-x-10"
                 style="font-family: lalezar;"
             >
                 {{ $t('webDeveloper') }}
