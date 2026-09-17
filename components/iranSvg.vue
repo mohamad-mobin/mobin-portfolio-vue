633b66d17ebee31918bbfa096252a7910fd78e79
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-17 21:22:03 +0330

    add : import 2 svg to component

---
+++ components/iranSvg.vue
@@ -0,0 +1,26 @@
+<template>
+            <svg
+          c=""
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="25px"
+          height="40px"
+          viewBox="0 0 36 36"
+          aria-hidden="true"
+          role="img"
+          class="iconify iconify--twemoji"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path fill="#DA0001" d="M0 27a4 4 0 0 0 4 4h28a4 4 0 0 0 4-4v-4H0v4z">
+          </path>
+          <path fill="#EEE" d="M0 13h36v10H0z">
+          </path>
+          <path fill="#239F40" d="M36 13V9a4 4 0 0 0-4-4H4a4 4 0 0 0-4 4v4h36z">
+          </path>
+
+          <g fill="#BE1931">
+
+          </g>
+
+        </svg>
+</template>
