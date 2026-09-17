169ac9935832d86e8cc02602d2e741e7e07f9510
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-17 20:19:05 +0330

    fix : indent of miniSideBar & toggleLang & tools&techs components

--- components/tools&techs.vue
+++ components/tools&techs.vue
@@ -1,26 +1,28 @@
 <template>
-   <div data-aos="flip-left"
-   :class="hoverBorder"
-                                class="skill-card group relative overflow-hidden
-                                        bg-[#131414] border border-[#3a3a3d]
-                                        sm:w-50 sm:h-50 w-42.5 h-42.5
-                                        sm:p-6 p-4 rounded-3xl flex flex-col justify-center items-center
-                                        transition-all duration-300 hover:shadow-lg hover:shadow-orange-900/50 toolsBox">
-                                                    
-                                <p class="font-semibold pb-3 text-lg">{{ techName }}</p>
-                                            
-                                <div class="flex flex-col items-center justify-center space-y-2">
-                                    <div class="relative items-center justify-center flex w-24 h-24 sm:w-28 sm:h-28">
-                                        <img class=" size-22 bottom-2 right-2" :src="props.imgSrc" alt="">
-                                    </div>
-                                </div>
+  <div
+    data-aos="flip-left"
+    :class="hoverBorder"
+    class="skill-card group relative overflow-hidden
+           bg-[#131414] border border-[#3a3a3d]
+           sm:w-50 sm:h-50 w-42.5 h-42.5
+           sm:p-6 p-4 rounded-3xl flex flex-col justify-center items-center
+           transition-all duration-300 hover:shadow-lg hover:shadow-orange-900/50 toolsBox"
+  >
+    <p class="font-semibold pb-3 text-lg">{{ techName }}</p>
 
-                                <div 
-                                :class="boxGradiant"
-                                class="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-                                        <span class="absolute text-[#FFB400] right-2 bottom-2 text-xl font-bold">{{ percent }}%</span>
-                                
-                            </div>
+    <div class="flex flex-col items-center justify-center space-y-2">
+      <div class="relative items-center justify-center flex w-24 h-24 sm:w-28 sm:h-28">
+        <img class="size-22 bottom-2 right-2" :src="props.imgSrc" alt="">
+      </div>
+    </div>
+
+    <div
+      :class="boxGradiant"
+      class="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+    ></div>
+
+    <span class="absolute text-[#FFB400] right-2 bottom-2 text-xl font-bold">{{ percent }}%</span>
+  </div>
 </template>
 
 <script setup>
