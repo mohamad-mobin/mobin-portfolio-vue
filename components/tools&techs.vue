60e538d3dff616e162afc33870cbac7d852122b3
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-17 20:38:10 +0330

    update : change figma & bashscript image

--- components/tools&techs.vue
+++ components/tools&techs.vue
@@ -12,7 +12,7 @@
 
     <div class="flex flex-col items-center justify-center space-y-2">
       <div class="relative items-center justify-center flex w-24 h-24 sm:w-28 sm:h-28">
-        <img class="size-22 bottom-2 right-2" :src="props.imgSrc" alt="">
+        <img class="size-22 object-contain bottom-2 right-2" :src="props.imgSrc" alt="">
       </div>
     </div>
 
