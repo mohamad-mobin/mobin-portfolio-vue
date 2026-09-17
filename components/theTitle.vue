15e18e075a256466107a99a263f8453d4c6a55e3
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-17 20:12:28 +0330

    fix : remove warning of theTitle.vue component

--- components/theTitle.vue
+++ components/theTitle.vue
@@ -4,7 +4,7 @@
 
         <div data-aos="flip-left" data-aos-offset="0" data-aos-duration="2000" class="relative text-center">
 
-            <p id="myProjects" class="absolute text-4xl md:text-5xl w-full justify-center items-center top-[19px] md:top-8 left-1/2 flex gap-x-2 transform -translate-x-1/2">
+            <p id="myProjects" class="absolute text-4xl md:text-5xl w-full justify-center items-center top-4.75 md:top-8 left-1/2 flex gap-x-2 transform -translate-x-1/2">
 
                 <span class="text-white aboutParagraph">{{ titleContentArray[0] }}</span>
                 <span class="text-[#FFB400]">{{ titleContent[1] }}</span>
