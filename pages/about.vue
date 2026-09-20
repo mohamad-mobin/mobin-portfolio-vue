84c443dc570612ed78c829e77dcc330eb83d9b53
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-20 23:19:40 +0330

    test : comment tools&techs in about page

--- pages/about.vue
+++ pages/about.vue
@@ -120,15 +120,13 @@
                 </a>
             </div>
         </div>
-
+<!-- 
         <TheTitle :titleContent="['My','Techs', 'FrameWork']" />
 
         <div>
             <div style="font-family: poppins;" class="flex flex-wrap justify-center gap-5 px-5 pt-5 text-center text-white max-w-7xl mx-auto z-50">
              
-                <!-- html | css -->
                 <div class="flex gap-5">
-                    <!-- html -->
                         <div class="hover:scale-110 duration-300">
                            <Tools&techs
                             boxShadow="hover:shadow-orange-500/50"
@@ -139,7 +137,6 @@
                            :techName="'Html'" />
                         </div>
 
-                        <!-- css -->
                        <div class="hover:scale-110 duration-300">
                             <Tools&techs
                         boxShadow="hover:shadow-blue-500/50"
@@ -151,9 +148,7 @@
                         </div>
                 </div>
                     
-                <!-- tailwind css | javascript -->
                 <div class="flex gap-5">
-                    <!-- tailwind css -->
                     <div class="hover:scale-110 duration-300">
                         <Tools&techs
                         boxShadow="hover:shadow-sky-500/50"
@@ -164,7 +159,6 @@
                                 :techName="'TailwindCss'" />
                     </div>
 
-                    <!-- javascript -->
                     <div class="hover:scale-110 duration-300">
                         <Tools&techs
                         boxShadow="hover:shadow-yellow-500/50"
@@ -236,6 +230,7 @@
 
         <TheTitle :titleContent="['My','Tools', 'Utensils']" />
 
+        
 
         <div style="font-family: poppins;" class="flex flex-wrap justify-center gap-5 px-5 pt-5 md:pb-5 text-center text-white max-w-7xl mx-auto z-50">
 
@@ -358,7 +353,7 @@
                         </div>
                     </div>
         </div>
-
+ -->
 
     
      
@@ -414,23 +409,23 @@ import { stopLenis, startLenis } from '@/utils/lenis.js'
 import MiniSideBar from "../components/miniSideBar.vue"
 import TheTitle from "../components/theTitle.vue"
 
-import htmlImg from '~/assets/images/tools&techs-image/html.png'
-import cssImg from '~/assets/images/tools&techs-image/css.png'
-import tailwindImg from '~/assets/images/tools&techs-image/tailwind.png'
-import javascriptImg from '~/assets/images/tools&techs-image/js.png'
-import vueImg from '~/assets/images/tools&techs-image/vue.png'
-import vuetifyImg from '~/assets/images/tools&techs-image/vuetify.png'
-import nuxtImg from '~/assets/images/tools&techs-image/nuxt.png'
-import typescriptImg from '~/assets/images/tools&techs-image/typescript.png'
-import gitImg from '~/assets/images/tools&techs-image/git.png'
-import githubImg from '~/assets/images/tools&techs-image/github.png'
-import linuxImg from '~/assets/images/tools&techs-image/linux.png'
-import ubuntuImg from '~/assets/images/tools&techs-image/ubuntu.png'
-import vscodeImg from '~/assets/images/tools&techs-image/vscode.png'
-import postmanImg from '~/assets/images/tools&techs-image/postman.png'
-import figmaImg from '~/assets/images/tools&techs-image/figma.png'
-import bashscriptImg from '~/assets/images/tools&techs-image/bashscript.png'
-import ffmpegImg from '~/assets/images/tools&techs-image/ffmpeg.png'
+// import htmlImg from '~/assets/images/tools&techs-image/html.png'
+// import cssImg from '~/assets/images/tools&techs-image/css.png'
+// import tailwindImg from '~/assets/images/tools&techs-image/tailwind.png'
+// import javascriptImg from '~/assets/images/tools&techs-image/js.png'
+// import vueImg from '~/assets/images/tools&techs-image/vue.png'
+// import vuetifyImg from '~/assets/images/tools&techs-image/vuetify.png'
+// import nuxtImg from '~/assets/images/tools&techs-image/nuxt.png'
+// import typescriptImg from '~/assets/images/tools&techs-image/typescript.png'
+// import gitImg from '~/assets/images/tools&techs-image/git.png'
+// import githubImg from '~/assets/images/tools&techs-image/github.png'
+// import linuxImg from '~/assets/images/tools&techs-image/linux.png'
+// import ubuntuImg from '~/assets/images/tools&techs-image/ubuntu.png'
+// import vscodeImg from '~/assets/images/tools&techs-image/vscode.png'
+// import postmanImg from '~/assets/images/tools&techs-image/postman.png'
+// import figmaImg from '~/assets/images/tools&techs-image/figma.png'
+// import bashscriptImg from '~/assets/images/tools&techs-image/bashscript.png'
+// import ffmpegImg from '~/assets/images/tools&techs-image/ffmpeg.png'
 
 
 const currentPageConst = "about"
