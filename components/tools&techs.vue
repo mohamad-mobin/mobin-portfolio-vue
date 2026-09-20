8494898fc889a22d038654f592f0ab75de9c33f2
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-20 22:08:44 +0330

    update : change size of tools&techs box

--- components/tools&techs.vue
+++ components/tools&techs.vue
@@ -4,15 +4,15 @@
     :class="hoverBorder"
     class="skill-card group relative overflow-hidden
            bg-[#131414] border border-[#3a3a3d]
-           sm:w-50 sm:h-50 w-42.5 h-42.5
+           sm:w-50 sm:h-50 w-40 h-40
            sm:p-6 p-4 rounded-3xl flex flex-col justify-center items-center
            transition-all duration-300 hover:shadow-lg hover:shadow-orange-900/50 toolsBox"
   >
     <p class="font-semibold pb-3 text-lg">{{ techName }}</p>
 
     <div class="flex flex-col items-center justify-center space-y-2">
-      <div class="relative items-center justify-center flex w-24 h-24 sm:w-28 sm:h-28">
-        <img class="size-22 object-contain bottom-2 right-2" :src="props.imgSrc" alt="">
+      <div class="relative items-center justify-center flex ">
+        <img class="size-17 sm:size-22 object-contain bottom-2 right-2" :src="props.imgSrc" alt="">
       </div>
     </div>
 
