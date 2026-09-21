ee90089dd44c099c7837bdb790aca562aa3911d4
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-21 18:23:22 +0330

    test: my name and job, to poppins

--- pages/index.vue
+++ pages/index.vue
@@ -42,13 +42,13 @@
                 id="mobin"
                 class="block text-[#FFB400] text-4xl md:text-[30px] lg:text-[5vw] relative duration-300 group-hover:-translate-x-5 group-hover:scale-95
                     before:absolute before:hidden md:before:block before:h-1 before:w-7 lg:before:w-10 before:bg-[#FFB400] before:rounded-2xl before:bottom-6 lg:before:bottom-8 before:-left-10 lg:before:-left-16"
-                style="font-family: lalezar;"
+                style="font-family: poppins;"
             >
                 {{ $t('imMobinRezaee') }}
             </span>
             <span
                 class="block text-white text-[25px] lg:text-[3vw] duration-300 group-hover:translate-x-10"
-                style="font-family: lalezar;"
+                style="font-family: poppins;"
             >
                 {{ $t('webDeveloper') }}
             </span>
