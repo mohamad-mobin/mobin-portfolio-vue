2ee944fe94ccabb04bc5621ec37ac0155078d0a0
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-21 13:42:42 +0330

    update : change fontFamily of title & remove some class And id , in theTitle.vue

--- components/theTitle.vue
+++ components/theTitle.vue
@@ -4,10 +4,10 @@
 
         <div data-aos="flip-left" data-aos-offset="0" data-aos-duration="2000" class="relative text-center">
 
-            <p id="myProjects" class="absolute text-4xl md:text-5xl w-full justify-center items-center top-4.75 md:top-8 left-1/2 flex gap-x-2 transform -translate-x-1/2">
+            <p class="absolute text-4xl md:text-5xl w-full justify-center items-center top-4.75 md:top-8 left-1/2 flex gap-x-2 transform -translate-x-1/2">
 
-                <span class="text-white aboutParagraph">{{ titleContentArray[0] }}</span>
-                <span class="text-[#FFB400]">{{ titleContent[1] }}</span>
+                <span style="font-family: lalezar;" class="text-white tracking-wide text-6xl">{{ titleContentArray[0] }}</span>
+                <span style="font-family: lalezar;" class="text-[#FFB400] tracking-wide text-6xl">{{ titleContent[1] }}</span>
             </p>
 
             <p id="myWorks" class="absolute text-6xl md:text-8xl text-[#222222] -z-10 left-1/2 transform -translate-x-1/2">
