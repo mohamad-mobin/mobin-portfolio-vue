1a17fbad18de234fc42575e46fdc729889148ea6
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-22 13:29:46 +0330

    refactor: move projectsHint logic from index.vue to home/projectsHint.vue

--- pages/index.vue
+++ pages/index.vue
@@ -35,13 +35,7 @@
     
         </div>
 
-    <div class="flex justify-center z-50 items-center w-fit h-16 fixed bottom-5 left-[60%] md:left-auto md:right-28 md:top-[52%] my-10 flashy-bounce arrow">
-            <div class="relative bg-[#FFB400] w-14 border-2 border-white/50 h-8 rounded-full flex justify-center items-center">
-                <span class="relative bottom-px text-[11px] font-bold select-none text-black projectsHint" style="font-family: poppins,vazir;">{{ $t('bounce') }}</span>
-                
-            </div>
-               
-    </div>
+        <ProjectsHint />
 
         <MiniSideBar :currentPage="currentPageConst"/>
 
@@ -52,6 +46,7 @@ import { onMounted, ref } from "vue"
 import MyPic from "../components/home/myPic.vue"
 import MyDescription from "../components/home/myDescription.vue"
 import CallToAction from "../components/home/callToAction.vue"
+import ProjectsHint from "../components/home/projectsHint.vue"
 
 useHead({
     title: "Home",
