1f201c0dd9744560831a4355fc6cd7bc9e3578e8
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-22 13:48:02 +0330

    style: change hover:scale of callToAction elements

--- components/home/callToAction.vue
+++ components/home/callToAction.vue
@@ -2,7 +2,7 @@
   <div>
           <div class="flex flex-col md:flex-row w-100 md:w-fit h-62.5 md:h-22.5 gap-3 md:gap-x-5 items-center">
               <div data-aos="zoom-out-right" data-aos-delay="600" class="flex justify-center items-center">
-                  <NuxtLink to="/about" class="text-center md:ml-10 mt-4 py-4 pl-7 pr-20 rounded-full overflow-hidden ring-1 ring-[#FFB400] self-center relative z-20 before:absolute before:size-0 before:rounded-full before:-z-10 before:duration-300 before:bg-[#FFB400] hover:before:size-75 before:top-1/2 before:left-1/2 before:-translate-x-1/2 bg-[#222121] before:-translate-y-1/2 hover:scale-110 duration-300 cursor-none" style="font-family: poppins,vazir;">
+                  <NuxtLink to="/about" class="text-center md:ml-10 mt-4 py-4 pl-7 pr-20 rounded-full overflow-hidden ring-1 ring-[#FFB400] self-center relative z-20 before:absolute before:size-0 before:rounded-full before:-z-10 before:duration-300 before:bg-[#FFB400] hover:before:size-75 before:top-1/2 before:left-1/2 before:-translate-x-1/2 bg-[#222121] before:-translate-y-1/2 hover:scale-115 duration-300 cursor-none" style="font-family: poppins,vazir;">
                       {{ $t('aboutMe') }}
                       <button class="bg-[#FFB400] p-3.25 rounded-full absolute top-px right-0 overflow-hidden cursor-none">
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
@@ -16,7 +16,7 @@
     
             <div data-aos="zoom-out-left" data-aos-delay="600" class="flex justify-center items-center">
                 
-                <NuxtLink to="/projects" class="text-center mt-4 py-4 pl-7 pr-20 rounded-full overflow-hidden ring-1 ring-[#FFB400] self-center relative z-20 before:absolute before:size-0 before:rounded-full before:duration-300 before:bg-[#FFB400] hover:before:size-75 before:top-1/2 before:left-1/2 bg-[#222121] before:-translate-x-1/2 before:-translate-y-1/2 before:-z-10 animate-bounce hover:scale-110 duration-300 cursor-none" style="font-family: poppins,vazir;">
+                <NuxtLink to="/projects" class="text-center mt-4 py-4 pl-7 pr-20 rounded-full overflow-hidden ring-1 ring-[#FFB400] self-center relative z-20 before:absolute before:size-0 before:rounded-full before:duration-300 before:bg-[#FFB400] hover:before:size-75 before:top-1/2 before:left-1/2 bg-[#222121] before:-translate-x-1/2 before:-translate-y-1/2 before:-z-10 animate-bounce hover:scale-115 duration-300 cursor-none" style="font-family: poppins,vazir;">
                     {{ $t('myProjects') }}
                     <button class="bg-[#FFB400] p-3.25 rounded-full absolute top-px right-0 overflow-hidden cursor-none">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
