1aadfedc222dcbd14facf62ac08b354051b430dd
mohamad-mobin <RealMobinRezaee@gmail.com>
2026-09-26 20:05:11 +0330

    fix: z-index of myPic

--- components/home/myPic.vue
+++ components/home/myPic.vue
@@ -2,8 +2,8 @@
   <div>
                      <div data-aos="fade-up"
                  data-aos-delay="700"
-                 class="rounded-3xl overflow-hidden md:fixed md:left-[5%] w-75 md:w-[30%] h-100 md:h-[90%] top-[5%] shadow-md ring-[1px] ring-[#3a3a3d] z-40 relative" id="ManImage">
-                    <div class="absolute bg-[#3a3a3d] flex z-50 justify-center items-center overflow-hidden" ref="cinemaScreen">
+                 class="rounded-3xl overflow-hidden md:fixed md:left-[5%] w-75 md:w-[30%] h-100 md:h-[90%] top-[5%] shadow-md ring-[1px] ring-[#3a3a3d] z-30 relative" id="ManImage">
+                    <div class="absolute bg-[#3a3a3d] flex z-30 justify-center items-center overflow-hidden" ref="cinemaScreen">
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-32 animate-bounce text-[#FFB400]"><path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" /></svg>
 
